da4f95fc78d0d1f5767eb311835931c59f3963f5
Iván Litteri <ilitteri@fi.uba.ar>
2022-02-25 19:20:24 -0300

    Add an endpoint with a query param
    This is unfinished, the persistance of the data must be tested before merging

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
-fn main() {
-    println!("Hello, world!");
+use rocksdb::DB;
+use std::collections::HashMap;
+use warp::Filter;
+
+#[tokio::main]
+async fn main() {
+    // GET /shorten?url=<url>
+    let shorten = warp::get()
+        .and(warp::path("shorten"))
+        .and(warp::query::<HashMap<String, String>>().map(|query: HashMap<String, String>| {
+            let url = query["url"].clone();
+            let storage = DB::open_default("rocks.db").unwrap();
+            // TODO: Wrap url.clone() with actual shorten_link function
+            let shortened_url = url.clone();
+            storage.put(&shortened_url, &url).expect("Failed to insert");
+            println!("{:?}", storage);
+            shortened_url
+        }));
+
+    warp::serve(shorten).run(([127, 0, 0, 1], 8080)).await;
 }
