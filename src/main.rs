ace8f9ee1bdfa5fff5b9d9879b86844f10437c5d
IAvecilla <iavecilla@fi.uba.ar>
2022-02-25 14:09:48 -0300

    New https endpoint and implement the redirect from the http endpoint, add the certificates

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,19 @@
-use warp::Filter;
+use futures::future;
+use warp::{hyper::Uri, Filter};
 
 #[tokio::main]
 async fn main() {
     //pretty_env_logger::init();
 
-    // GET /ping
-    let ping = warp::path("ping").map(|| "Pong!");
+    let http_route = warp::path("ping").map(|| warp::redirect(Uri::from_static("https://127.0.0.1:8080")));
 
-    let routes = warp::get().and(ping);
+    let https_routes = warp::path("ping").map(|| "Connected through tls");
+    let (_http_addr, http_warp) = warp::serve(http_route).bind_ephemeral(([127, 0, 0, 1], 3030));
+    let (_https_addr, https_warp) = warp::serve(https_routes)
+        .tls()
+        .cert_path("tls/MyCertificate.crt")
+        .key_path("tls/MyKey.key")
+        .bind_ephemeral(([127, 0, 0, 1], 8080));
 
-    warp::serve(routes).run(([127, 0, 0, 1], 3030)).await;
+    future::join(http_warp, https_warp).await;
 }
