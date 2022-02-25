1ada3fc9e278b1042553bd50af19d07a24493e47
Martin Fraga <martin.fraga@lambdaclass.com>
2022-02-25 12:24:09 -0300

    tested endpoint

--- src/main.rs
+++ src/main.rs
@@ -3,13 +3,11 @@ use warp::Filter;
 #[tokio::main]
 async fn main() {
     //pretty_env_logger::init();
-    
+
     // GET /ping
     let ping = warp::path("ping").map(|| "Pong!");
 
-    let routes = warp::get().and(
-        ping,
-    );
+    let routes = warp::get().and(ping);
 
     warp::serve(routes).run(([127, 0, 0, 1], 3030)).await;
 }
