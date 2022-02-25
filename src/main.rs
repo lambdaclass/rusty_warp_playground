b96d4e172956b654d0d20b014fcbcca509835249
Martin Fraga <martin.fraga@lambdaclass.com>
2022-02-25 12:22:19 -0300

    add endpoint

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
-fn main() {
-    println!("Hello, world!");
+use warp::Filter;
+
+#[tokio::main]
+async fn main() {
+    //pretty_env_logger::init();
+    
+    // GET /ping
+    let ping = warp::path("ping").map(|| "Pong!");
+
+    let routes = warp::get().and(
+        ping,
+    );
+
+    warp::serve(routes).run(([127, 0, 0, 1], 3030)).await;
 }
